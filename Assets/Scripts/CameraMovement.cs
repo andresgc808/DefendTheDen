8555a5917bc01895886191eeb4d5ce50ac924393
andresgc <andresgc7898@gmail.com>
2024-12-16 20:39:44 -0700

    Camera Movement Script Implementation

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    [SerializeField] private Transform player;
+    [SerializeField] private float smoothSpeed = 0.1f;
+    private Vector3 _offset = new Vector3(0, 10.0f, 0);
+    private Vector3 _currentVelocity = Vector3.zero;
+
+    private void LateUpdate() {
+        transform.position = new Vector3(player.transform.position.x, player.transform.position.y, player.transform.position.z);
+    }
+}
