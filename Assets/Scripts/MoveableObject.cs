f7c0ddab67273136d3bce7905a9aa48efba6df92
andresgc <andresgc7898@gmail.com>
2024-12-24 16:34:36 -0700

    Added Rhino 3D Model and Configured animation, also added look rotation logic for moveable objects

--- Assets/Scripts/MoveableObject.cs
+++ Assets/Scripts/MoveableObject.cs
@@ -9,6 +9,20 @@ public class MoveableObject : MonoBehaviour {
     [SerializeField] public bool isMoving = false;
     private Vector3 _moveTarget;
     private bool useHorizontalGrid = true;
+    //private Animator _animator;
+    [SerializeField] private float _rotationSpeed = 15f;
+    private Matrix4x4 matrix = Matrix4x4.Rotate(Quaternion.Euler(0, 45, 0));
+    [SerializeField] private GameObject _forwardDirectionObject;
+    private Transform _forwardDirection;
+    [SerializeField] private Animator _animator;
+
+    private void Start() {
+        //_animator = GetComponent<Animator>();
+        if (_forwardDirectionObject == null)
+            _forwardDirection = this.transform;
+        else
+            _forwardDirection = _forwardDirectionObject.transform; // sets forward if null
+    }
 
     public void StartMovement(Vector3 targetPosition) {
         if (isMoving == true) {
@@ -22,10 +36,16 @@ public class MoveableObject : MonoBehaviour {
 
         isMoving = true;
 
+        if (_animator != null)
+            _animator.SetBool("isMoving", true);
+
     }
 
     public void StopMovement() {
         isMoving = false;
+
+        if (_animator != null)
+            _animator.SetBool("isMoving", false);
 
         var body = GetComponent<Rigidbody>();
 
@@ -58,6 +78,13 @@ public class MoveableObject : MonoBehaviour {
 
 
         transform.position += (dir * Time.fixedDeltaTime) * movementSpeed;
+
+        //var skewedDir = matrix.MultiplyPoint3x4(dir);
+        //var skewedInput = matrix.MultiplyPoint3x4(_forwardDirection.up);
+        //Rotation code start here:
+        Quaternion targetRotation = Quaternion.LookRotation(dir, _forwardDirection.up); // calculates new rotation to look at that direction
+        
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, _rotationSpeed * Time.deltaTime);
 
 
     }
