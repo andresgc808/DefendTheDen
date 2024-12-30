d4ad5ba1530e1ac3edf945195b77ee6e246e29bd
andresgc <andresgc7898@gmail.com>
2024-12-30 13:32:09 -0700

    AI Navmesh and Navmesh surface experimentation (See Description) (New movement logic)
    New movement script for Animals with new RTS style clickable logic. Shift select to select multiple units.
    
    Animals must have their "SelectionIndicator" as their first child, it will be enabled when selected to show that the animal is selected.
    
    A Ground Marker game object now exists which shows where the user clicked for movement.
    
    MouseTarget now uses the Unit Selection Manager script and has MousePosition3D deactivated.
    
    Following commits will look into fully implementing navMesh movement.

---
+++ Assets/Scripts/AI_Scipts/UnitMovement.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class UnitMovement : MonoBehaviour
+{
+    public LayerMask ground;
+    NavMeshAgent agent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(1)) {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity, ground)) {
+                agent.SetDestination(hit.point);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/AI_Scipts/UnitSelectionManager.cs
@@ -0,0 +1,93 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitSelectionManager : MonoBehaviour {
+    public static UnitSelectionManager Instance { get; set; }
+
+    public List<GameObject> allUnitsList = new List<GameObject>();
+    public List<GameObject> selectedUnitsList = new List<GameObject>();
+
+    [SerializeField] private Camera _camera;
+
+    public LayerMask animal;
+    public LayerMask ground;
+    public GameObject groundMarker;
+
+    private void Awake() {
+        if (Instance != null && Instance != this) {
+            Destroy(this.gameObject);
+        } else {
+            Instance = this;
+        }
+    }
+
+    private void Update() {
+        if (Input.GetMouseButtonDown(0)) {
+            Ray ray = _camera.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity, animal)) {
+
+                if (Input.GetKey(KeyCode.LeftShift)) {
+                    MultiSelect(hit.collider.gameObject);
+                } else {
+                    Debug.Log(hit.collider.gameObject);
+                    SelectObject(hit.collider.gameObject);
+                }
+            } else {
+                // deselect all units
+                DeselectAll();
+            }
+        } 
+
+        if (Input.GetMouseButtonDown(1)) {
+            Ray ray = _camera.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity, ground)) {
+                groundMarker.transform.position = hit.point;
+
+                // trick to make animation later
+                groundMarker.SetActive(false);
+                groundMarker.SetActive(true);
+            }
+        }
+    }
+
+    private void SelectObject(GameObject obj) {
+        DeselectAll();
+        selectedUnitsList.Add(obj);
+        TriggerSelectionIndicator(obj, true);
+        EnableUnitMovement(obj, true);
+
+    }
+
+    private void DeselectAll() {
+        foreach (GameObject obj in selectedUnitsList) {
+            EnableUnitMovement(obj, false);
+            TriggerSelectionIndicator(obj, false);
+        }
+        selectedUnitsList.Clear();
+    }
+
+    private void EnableUnitMovement(GameObject obj, bool shouldMove) {
+        obj.GetComponent<UnitMovement>().enabled = shouldMove;
+    }
+
+    private void TriggerSelectionIndicator(GameObject obj, bool isSelected) {
+        obj.transform.GetChild(0).gameObject.SetActive(isSelected);
+    }
+
+    private void MultiSelect(GameObject obj) {
+        if (selectedUnitsList.Contains(obj)) {
+            selectedUnitsList.Remove(obj);
+            TriggerSelectionIndicator(obj, false);
+            EnableUnitMovement(obj, false);
+        } else {
+            selectedUnitsList.Add(obj);
+            TriggerSelectionIndicator(obj, true);
+            EnableUnitMovement(obj, true);
+        }
+    }
+}
