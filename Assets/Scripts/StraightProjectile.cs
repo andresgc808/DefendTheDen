712d4409b8cb8d218ea59bfdc731b4bed5d4127b
andresgc <andresgc7898@gmail.com>
2025-01-03 16:02:40 -0700

    added basic poison trait

--- Assets/Scripts/StraightProjectile.cs
+++ Assets/Scripts/StraightProjectile.cs
@@ -1,3 +1,4 @@
+#nullable enable
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,14 +7,32 @@ public class StraightProjectile : BaseProjectile {
     [SerializeField] public float speed = 10f;
     [SerializeField] public float collisionRadius = 1f;
     [SerializeField] public LayerMask whatIsEnemy;
+    private GameObject? _effectObject;
+    private Trait? _appliedTrait;
 
     public override void LaunchProjectile(Vector3 startingPosition, Vector3 targetPosition, float damage) {
         transform.position = startingPosition;
-            Debug.Log("Projectile created, at " + transform.position);
+        Debug.Log("Projectile created, at " + transform.position);
         StartCoroutine(MoveProjectile(targetPosition, damage));
     }
 
-    private IEnumerator MoveProjectile( Vector3 targetPosition, float damage) {
+    public override void ApplyTrait(Trait trait) {
+        _appliedTrait = trait;
+        if (_appliedTrait != null && _appliedTrait.particlePrefab != null) {
+
+            _effectObject = Instantiate(_appliedTrait.particlePrefab, transform); // attach the effect to the projectile
+            StartCoroutine(DestroyParticle(_appliedTrait.effectDuration)); // destroys particle after some time
+        }
+    }
+
+    private IEnumerator DestroyParticle(float duration) {
+        yield return new WaitForSeconds(duration);
+
+        if (_effectObject != null)
+            Destroy(_effectObject); // destroy the visual particle effect
+    }
+
+    private IEnumerator MoveProjectile(Vector3 targetPosition, float damage) {
 
         Vector3 direction = (targetPosition - transform.position).normalized;
 
@@ -27,14 +46,25 @@ public class StraightProjectile : BaseProjectile {
                 var damageableTarget = hitCollider.GetComponent<IDamageable>();
                 if (damageableTarget != null && damageableTarget.IsAlive && !damageableTarget.Equals(this.GetComponent<IDamageable>())) {
                     damageableTarget.TakeDamage(damage); // deal damage to target
-
+                    if (_appliedTrait != null) {
+                        StartCoroutine(ApplyPoison(damageableTarget, _appliedTrait.duration, _appliedTrait.damagePerSecond));
+                    }
                     Debug.Log("Projectile Destroyed: " + gameObject.name);
-                    DestroyProjectile(); // destroy projectile when collided with target.
-                    yield break; // stop coroutine
+                    DestroyProjectile();
+                    yield break;
                 }
             }
             yield return null;
         }
         DestroyProjectile();
+    }
+
+    private IEnumerator ApplyPoison(IDamageable target, float duration, float damagePerSecond) {
+        float timeElapsed = 0;
+        while (timeElapsed <= duration) {
+            target.TakeDamage(damagePerSecond * Time.deltaTime);
+            timeElapsed += Time.deltaTime;
+            yield return null;
+        }
     }
 }
